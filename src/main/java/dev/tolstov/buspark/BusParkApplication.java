e448c4b346e445e653d31eed7fe062b3110209ea
itolstov09 <itolstov09@gmail.com>
2022-11-07 17:57:57 +0600

    build: Spring Initializr

---
+++ src/main/java/dev/tolstov/buspark/BusParkApplication.java
@@ -0,0 +1,13 @@
+package dev.tolstov.buspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusParkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tolstov/buspark/BusParkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tolstov.buspark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

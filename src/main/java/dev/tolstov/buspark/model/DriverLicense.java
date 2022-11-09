cfd205c17c93d4580290e21705ad259bcadafa20
itolstov09 <itolstov09@gmail.com>
2022-11-09 17:36:22 +0600

    feat: embedded DriverLicense

---
+++ src/main/java/dev/tolstov/buspark/model/DriverLicense.java
@@ -0,0 +1,19 @@
+package dev.tolstov.buspark.model;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter @Setter
+@Embeddable
+public class DriverLicense {
+    @NonNull
+    private String licenseID;
+    //TODO переделать setter. сделать возможность передавать список категорий, который потом будет переводится в стрoку
+    @NonNull
+    @Column(name = "license_categories")
+    private String licenseCategories;
+}

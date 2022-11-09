2017f299fdd6de4e84c23788f91d4707d7e6e034
itolstov09 <itolstov09@gmail.com>
2022-11-09 17:37:37 +0600

    feat: Employee driver license functionality

--- src/main/java/dev/tolstov/buspark/model/Employee.java
+++ src/main/java/dev/tolstov/buspark/model/Employee.java
@@ -1,8 +1,6 @@
 package dev.tolstov.buspark.model;
 
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -10,37 +8,45 @@ import javax.persistence.*;
 @NoArgsConstructor
 @RequiredArgsConstructor
 public class Employee {
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NonNull
+    @Getter @Setter
     private String name;
 
     @NonNull
+    @Getter @Setter
     private String lastName;
 
+    @Getter @Setter
     private String middleName;
 
     @NonNull
+    @Getter @Setter
     private Double salary;
 
     @NonNull
+    @Getter @Setter
     @ManyToOne(optional = false)
     @JoinColumn(name = "home_address_ID",
         foreignKey = @ForeignKey(name = "employee_home_address_ID_fkey"))
     private Address homeAddress;
 
     @NonNull
+    @Getter @Setter
     @Enumerated(EnumType.STRING)
     private Post post;
 
-    public void setHomeAddress(Address homeAddress) {
-        this.homeAddress = homeAddress;
-    }
+    @Embedded
+    @Getter
+    @Setter
+    private DriverLicense driverLicense;
 
-    public Address getHomeAddress() {
-        return homeAddress;
+    public boolean isCanBeDriver() {
+        return driverLicense != null;
     }
 
 

a7d79f1e966287da30b502917b0a612c0d820fd8
itolstov09 <itolstov09@gmail.com>
2022-11-17 13:53:30 +0600

    feat: add constrainViolation exception handler

--- src/main/java/dev/tolstov/buspark/exception/GlobalControllerAdvice.java
+++ src/main/java/dev/tolstov/buspark/exception/GlobalControllerAdvice.java
@@ -1,15 +1,46 @@
 package dev.tolstov.buspark.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.persistence.EntityExistsException;
+import javax.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
+
+
+    @ResponseBody()
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    String constraintViolationExceptionHandler(ConstraintViolationException exception) {
+        return exception.getMessage();
+    }
+
+
+//    Используется вместе с @Valid. Пока не нужен
+//    @ResponseBody
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public Map<String, String> handleValidationExceptions(
+//            MethodArgumentNotValidException ex) {
+//        Map<String, String> errors = new HashMap<>();
+//        ex.getBindingResult()
+//                .getAllErrors()
+//                .forEach((error) -> {
+//                    String fieldName = ((FieldError) error).getField();
+//                    String errorMessage = error.getDefaultMessage();
+//                    errors.put(fieldName, errorMessage);
+//                });
+//        return errors;
+//    }
 
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
